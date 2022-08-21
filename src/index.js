33ee9ad31cc2f15632520d742c4e48da9faffd9b
pr-maryanarty <maryana.dementeva@gmail.com>
2022-08-21 23:59:44 +0400

    done

--- src/index.js
+++ src/index.js
@@ -50,15 +50,15 @@ function formatDay(timeindex) {
 
 function displayForecast(response) {
   let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast").innterHTML;
+  let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
-      <div class="col-2">
+      <div class="col-2 forecast-column">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
           src="http://openweathermap.org/img/wn/${
